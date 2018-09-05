2c098f1f53d057d822fdbea24f606352e5fc09f3
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-09-05 17:12:00 -0400

    Completed k-means algorithm

--- algorithms-from-scratch/k-means/k-means.py
+++ algorithms-from-scratch/k-means/k-means.py
@@ -1,32 +1,49 @@
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('ggplot')
 import numpy as np
 
-X = np.array([[1, 2],
-              [1.5, 1.8],
-              [5, 8],
-              [8, 8],
-              [1, 0.6],
-              [9, 11]])
-
-plt.scatter(X[:,0], X[:,1], s=150)
-plt.show()
-
-
 class k_means:
+
     def __init__(self, k, tol, max_iter):
         self.k = k
         self.tol = tol
         self.max_iter = max_iter
 
+
     def fit(self, dataset):
-        self.centroids = []
+        self.centroids = {}
+
         for i in range(self.k):
-            self.centroids.append(dataset[i])
+            self.centroids[i] = dataset[i]
 
         for i in range(self.max_iter):
-            self.classifications = []
+            self.classifications = {}
 
             for i in range(self.k):
-                self.classifications.append([])
+                self.classifications[i] = []
+
+            for featureset in dataset:
+                distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classifications[classification].append(featureset)
+
+            prev_centroids = dict(self.centroids)
+
+            for classification in self.classifications:
+                self.centroids[classification] = np.average(self.classifications[classification],axis=0)
+
+            optimized = True
+
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid-original_centroid)/original_centroid*100.0) > self.tol:
+                    print(np.sum((current_centroid-original_centroid)/original_centroid*100.0))
+                    optimized = False
+
+            if optimized:
+                break
+
+
+    def predict(self, data):
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
